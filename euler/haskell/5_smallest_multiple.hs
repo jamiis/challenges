f414fbacd900e4036b4b3709ffb7d6d9de6538e5
Jamis Johnson <jamismanwaring@gmail.com>
2014-01-04 21:20:11 -0700

    wowza. speed up of a few orders of magnitude and much less complexity.

--- euler/haskell/5_smallest_multiple.hs
+++ euler/haskell/5_smallest_multiple.hs
@@ -1,3 +1,1 @@
-import Data.List
-main = print $ find (isDivisibleByOneThrough 20) [1..]
-    where isDivisibleByOneThrough m x = all (\a -> x `mod` a == 0) [1..m]
+main = print $ foldl lcm 2 [3..20]
